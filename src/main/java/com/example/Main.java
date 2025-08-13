96d7d1cd11e630974f2896c77c6ac68bd0a07807
Naveen Singh <ns37300@gmail.com>
2025-08-13 13:59:01 +0530

    feature added

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -12,6 +12,10 @@ public class Main {
         System.out.println(defaultGreeting);
         String employeeId = createEmployeeId(45);
         System.out.println("Employee ID: " + employeeId);
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Employee ID: " + employeeId);
         String name = "prasad";
         System.out.println(nameUpperCAse(name));
     }
